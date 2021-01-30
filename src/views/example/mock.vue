079e2c9f138dfde3f71bf0a5532cd28d24e1b937
YuTingtao <1224100678@qq.com>
2021-01-30 21:37:26 +0800

    update

--- src/views/example/mock.vue
+++ src/views/example/mock.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
+        <el-form :inline="true">
+            <el-form-item label="类型">
+                <el-select v-model="filter.type" placeholder="请选择类型" filterable clearable>
+                    <el-option label="全部" value=""></el-option>
+                    <el-option label="类型1" value="1"></el-option>
+                    <el-option label="类型2" value="2"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="关键词">
+                <el-input v-model="filter.keyword" placeholder="请输入关键字"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="getList();">查询</el-button>
+                <el-button type="primary">新增</el-button>
+            </el-form-item>
+        </el-form>
         <el-table :data="data" border>
             <el-table-column label="姓名" prop="name"></el-table-column>
             <el-table-column label="邮箱" prop="email"></el-table-column>
@@ -18,6 +34,10 @@ export default {
     components: { simplePager },
     data() {
         return {
+            filter: {
+                type: '',
+                keyword: ''
+            },
             data: [],
             total: 100,
             current: 1
