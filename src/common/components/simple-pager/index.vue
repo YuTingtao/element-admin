27e076bd6ebbc1256cd621a99ffd6d33124fa55a
YuTingtao <735126858@qq.com>
2021-03-20 17:59:26 +0800

    Update index.vue

--- src/common/components/simple-pager/index.vue
+++ src/common/components/simple-pager/index.vue
@@ -2,7 +2,13 @@
     <div class="simple-pager">
         <i :class="['simple-pager-item', 'el-icon-d-arrow-left', currentNum == 1? 'disable':'']" @click="toFirst"></i>
         <i :class="['simple-pager-item', 'el-icon-arrow-left', currentNum == 1? 'disable':'']" @click="toPrev"></i>
-        <span class="simple-pager-num">{{currentNum + '/' + pageCount}}</span>
+        <div class="simple-pager-num">
+            <input type="text" v-model="currentNum"
+                :style="{ 'width': 8 * String(currentNum).length + 4 + 'px' }"
+                @change="handleChange($event)"
+                @keyup.enter="$event.target.blur()">
+            <span>{{'/' + pageCount}}</span>
+        </div>
         <i :class="['simple-pager-item', 'el-icon-arrow-right', currentNum == pageCount? 'disable':'']" @click="toNext"></i>
         <i :class="['simple-pager-item', 'el-icon-d-arrow-right', currentNum == pageCount? 'disable':'']" @click="toEnd"></i>
     </div>
@@ -63,6 +69,18 @@ export default {
                 this.currentNum++;
                 this.$emit('currentChange', this.currentNum);
             }
+        },
+        handleChange(e) {
+            let value = e.target.value;
+            value = Number(value.replace(/\D/g,''));
+            if (value < 1) {
+                this.currentNum = 1;
+            } else if (value > this.pageCount) {
+                this.currentNum = this.pageCount || 1;
+            } else {
+                this.currentNum = value;
+            }
+            this.$emit('currentChange', this.currentNum);
         }
     }
 }
@@ -105,11 +123,25 @@ export default {
     }
     .simple-pager-num{
         display: inline-block;
-        padding: 0 6px;
-        line-height: 28px;
+        padding: 0 5px;
+        line-height: 22px;
         background: #fff;
         vertical-align: bottom;
-        border-top: 1px solid #e6e6e6;
-        border-bottom: 1px solid #e6e6e6;
+        border-top: 1px solid #ddd;
+        border-bottom: 1px solid #ddd;
+        input {
+            position: relative;
+            z-index: 1;
+            width: 12px;
+            min-width: 12px;
+            height: 20px;
+            line-height: 20px;
+            padding: 0 20px 0 0;
+            margin-right: -20px;
+            border: none;
+            text-align: center;
+            color: inherit;
+            background: transparent;
+        }
     }
 </style>
