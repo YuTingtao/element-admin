dd83b60a28428dada5e49e3bc283ac2c6a76b984
YuTingtao <1224100678@qq.com>
2021-03-23 22:47:27 +0800

    分页优化

--- src/common/components/pager/index.vue
+++ src/common/components/pager/index.vue
@@ -5,6 +5,7 @@
         <div class="com-pager-num">
             <input type="text" v-model="currentNum"
                 :style="{ 'width': 8 * String(currentNum).length + 4 + 'px' }"
+                @input="handleInput($event)"
                 @change="handleChange($event)"
                 @keyup.enter="$event.target.blur()">
             <span>{{'/' + pageCount}}</span>
@@ -70,9 +71,13 @@ export default {
                 this.$emit('currentChange', this.currentNum);
             }
         },
-        handleChange(e) {
+        handleInput(e) {
             let value = e.target.value;
-            value = Number(value.replace(/\D/g,''));
+            value = value.replace(/\D/g,'');
+            this.currentNum = value;
+        },
+        handleChange(e) {
+            let value = Number(e.target.value);
             if (value < 1) {
                 this.currentNum = 1;
             } else if (value > this.pageCount) {
